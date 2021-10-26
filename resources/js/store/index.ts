26e4248e9231ccd6c9f6542d2d9f4dbd0be62f51
今村 昌平 <s.imamura@ef-4.co.jp>
2021-10-26 20:01:35 +0900

    delete unneccesary packages

--- resources/js/store/index.ts
+++ resources/js/store/index.ts
@@ -1,19 +1,11 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import auth from './auth';
-import createPersistedState from 'vuex-persistedstate'
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
-        auth
+        // modules
     },
-    plugins: [createPersistedState(
-        {
-            key: 'vuex',  // 設定しなければ'vuex'
-            paths: ['auth'],  // 保存するモジュール：設定しなければ全部。
-            storage: window.sessionStorage,  // 設定しなければlocalStorage
-        }
-    )],
+    plugins: []
 });
